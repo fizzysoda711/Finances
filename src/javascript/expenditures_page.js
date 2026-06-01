e29f369cac13f045ad590564e5ee54ed4e0d4f4c
Ana Cabrera-Bello <anamcabrerabello@gmail.com>
2026-06-01 00:25:46 -0400

    added input menu for new expeditures

--- src/javascript/expenditures_page.js
+++ src/javascript/expenditures_page.js
@@ -1,5 +1,3 @@
-console.log("expenditures.js loaded");
-
 // import invoke (to call rust functions from js)
 const invoke = window.__TAURI__.core.invoke;
 
@@ -36,11 +34,47 @@ async function getDropdownCategories()
     });
 }
 
+// close and reset new expenditure creation fields
+async function closeNewExpendituresPopup()
+{
+    const newExpenditureMenu = document.querySelector(".new-expenditure-inputs");
+    const newExpenditureDropdown = document.querySelector(".expenditure-input-categories-dropdown");
+    const newExpenditureAmount = document.querySelector(".new-expenditure-input-amount-input");
+    const newExpenditureDate = document.querySelector(".new-expenditure-input-date-input");
+    const newExpenditureNote = document.querySelector(".new-expenditure-input-note-input");
+
+    newExpenditureMenu.classList.add("hidden");
+
+    newExpenditureDropdown.value = "";
+    newExpenditureAmount.value = "";
+    newExpenditureDate.value = "";
+    newExpenditureNote.value = "";
+
+    document.querySelector(".new-expenditure-button").classList.remove("hidden");
+}
+
 // ----- EXPENDITURES PAGE SPECIFIC BEHAVIORS ----- //
 
 // creating a new expenditure
 document.querySelector(".new-expenditure-button").addEventListener("click", async function() {
     document.querySelector(".new-expenditure-inputs").classList.remove("hidden");
+    document.querySelector(".new-expenditure-button").classList.add("hidden");
 
     await getDropdownCategories();
+});
+
+// formatting for new expenditure amount input field
+// budget inputs should only allow two decimal places and no letters
+const amountInput = document.querySelector(".new-expenditure-input-amount-input");
+amountInput.addEventListener("input", function () {
+    let digits = amountInput.value.replace(/\D/g, "");
+
+    let cents = Number(digits);
+
+    amountInput.value = (cents / 100).toFixed(2);
+});
+
+// closing new expenditure menu
+document.querySelector(".cancel-new-expenditure-button").addEventListener("click", async function() {
+    await closeNewExpendituresPopup();
 });
